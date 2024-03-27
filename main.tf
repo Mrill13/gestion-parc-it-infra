7be9c18f3b274b1e065ad4018cfbd7a12c22a657
Mrill13 <147081193+Mrill13@users.noreply.github.com>
2024-03-27 18:23:31 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,49 @@
+{
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.0"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "random_integer" "random_id" {
+  min = 1000
+  max = 9999
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-Idir-terraform-giardina-${random_integer.random_id.result}"
+  location = "France Central"  # Region France Central
+}
+
+resource "azurerm_app_service_plan" "asp" {
+  name                = "asp-Idir-terraform-giardina-${random_integer.random_id.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "Linux"
+
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+
+  reserved = true
+}
+
+resource "azurerm_app_service" "webapp" {
+  name                = "webapp-Idir-terraform-giardina-${random_integer.random_id.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.asp.id
+
+  site_config {
+    java_version    = "1.7"
+    java_container  = "TOMCAT"
+  }
+}
